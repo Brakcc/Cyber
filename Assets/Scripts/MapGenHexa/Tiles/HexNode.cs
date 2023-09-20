888b868a1431c970e4a86c1f661aa8b7376467e3
Brakcc <clement.strappazzon.marques@outlook.fr>
2023-09-20 18:40:17 +0200

    a peu pres tout

--- Assets/Scripts/MapGenHexa/Tiles/HexNode.cs
+++ Assets/Scripts/MapGenHexa/Tiles/HexNode.cs
@@ -17,7 +17,7 @@ public struct HexCoords : ICoords
     public HexCoords(int q, int r) {
         _q = q;
         _r = r;
-        Pos = _q * new Vector2(0, Sqrt3) + _r * new Vector2(1.5f, Sqrt3 / 2);
+        Pos = _q * new Vector2(0, Sqrt3/2) + _r * new Vector2(1.5f/2, Sqrt3 / 4);
     }
 
     public float GetDistance(ICoords other) => (this - (HexCoords)other).AxialLength();
