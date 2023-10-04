41cad5836360ee09735aed25b1f1fcf99475cf2f
PierreAC29 <118602610+PierreAC29@users.noreply.github.com>
2023-10-04 23:24:54 +0200

    rajout dictionnaire

--- Assets/Scripts/IA/IAManager.cs
+++ Assets/Scripts/IA/IAManager.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -5,10 +6,12 @@ using UnityEngine;
 public class IAManager: MonoBehaviour
 {
     List<Vector3> TargetsPos = new List<Vector3>();
-
+    
 
     public static IAManager instance;
+    Dictionary<int, Vector3> posibility = new Dictionary<int, Vector3>();
 
+    PathResult result = new PathResult();
 
     private void Awake()
     {
@@ -28,7 +31,21 @@ public class IAManager: MonoBehaviour
         foreach (var x in TargetsPos)
         {
             Debug.Log(x.ToString());
+            int posibilityValue = TargetsPos.Count;
+            posibility.Add(posibilityValue, position);
         }
+    }
+
+    public void Start()
+    {
+        int posibilityValue = TargetsPos.Count;
+        Debug.Log("test" + posibility[1]);
+        Debug.Log("test" + posibility[2]);
+
+
+        
+
+      //  result.GetPathTo(new Vector3Int(2,4,0));
     }
 
 }
