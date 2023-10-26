502dace6de778a563164c0f1ca2a359976ae5d89
CHALARON Pierre-antoine <p.chalaron@rubika-edu.com>
2023-10-26 19:11:01 +0200

    gameloop

--- Assets/Scripts/GameManagement/GameLoopManager.cs
+++ Assets/Scripts/GameManagement/GameLoopManager.cs
@@ -1,6 +1,35 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameLoopManager : MonoBehaviour
 {
+    #region fields
+    int PlayerPlay = 1;
+    int CountPerso1;
+    int CountPerso2;
+
+    #endregion
+
+    #region selectionPlayer
     
+    void OnSelectionPlayer()
+    {
+        if (PlayerPlay == 1)
+        {
+        }
+        if(PlayerPlay == 2)
+        {
+        }
+    }
+    
+    
+    #endregion
+
+    void OnPlayer1Play()
+    {
+
+    }
+    #region PlayerPlay
+
+    #endregion
 }
