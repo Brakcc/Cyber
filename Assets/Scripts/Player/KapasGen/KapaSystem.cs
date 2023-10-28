76c90dcc2a8508fe1380085e587508d7c591c9f7
Brakcc <clement.strappazzon.marques@outlook.fr>
2023-10-29 01:03:28 +0200

    pathfind pour les kapas
    permettre aux kapa de ne pas passer au travers des obstacles
    (comparaison avec pathfind)

---
+++ Assets/Scripts/Player/KapasGen/KapaSystem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KapaSystem
+{
+    #region fields
+    private PathResult kapaRange = new();
+    private List<Vector3Int> activeTiles = new();
+    #endregion
+
+    #region methodes
+
+    #endregion
+}
