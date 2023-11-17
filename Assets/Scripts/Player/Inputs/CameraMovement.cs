58ce0f644886aa161de74f13c25033493a5c572f
Brakcc <clement.strappazzon.marques@outlook.fr>
2023-11-17 13:22:18 +0100

    Move de Cam
    Bon par contre on perd la PixelPerfect Cam qui fait de la merde

---
+++ Assets/Scripts/Player/Inputs/CameraMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraMovement : MonoBehaviour
+{
+    #region fields
+    Camera cam;
+    Vector3 origingPos;
+    Vector3 diff;
+    bool isDragging;
+    #endregion
+
+    #region methodes
+    void Awake()
+    {
+        cam = Camera.main;
+    }
+
+    void LateUpdate()
+    {
+        if (!isDragging) return;
+
+        diff = GetMousePosition() - transform.position;
+        transform.position = origingPos - diff;
+    }
+
+    public void OnDrag(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started) { origingPos = GetMousePosition(); }
+        isDragging = ctx.started || ctx.performed;
+    }
+    Vector3 GetMousePosition() => cam.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+    #endregion
+}
